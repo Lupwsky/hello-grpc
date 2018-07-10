5b6b4a3a4f59274d2e831c01700f1479e9ceac21
v_pwlu <v_pwlu@wesure.cn>
2018-07-10 12:07:08 +0800

    仿写的 spring 注解方式配置

--- grpc-client/src/main/java/com/grpc/client/Application.java
+++ grpc-client/src/main/java/com/grpc/client/Application.java
@@ -1,12 +1,17 @@
 package com.grpc.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
     // 测试回滚1
+    private final static Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        logger.warn("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     }
 }
