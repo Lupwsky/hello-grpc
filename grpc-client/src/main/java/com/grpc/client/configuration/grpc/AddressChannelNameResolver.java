76a6a79700308d516045b63e79382cdd72e08d69
v_pwlu <v_pwlu@wesure.cn>
2018-07-16 20:34:26 +0800

    更改测试文件

--- grpc-client/src/main/java/com/grpc/client/configuration/grpc/AddressChannelNameResolver.java
+++ grpc-client/src/main/java/com/grpc/client/configuration/grpc/AddressChannelNameResolver.java
@@ -24,27 +24,67 @@ public class AddressChannelNameResolver extends NameResolver {
     private final Attributes attributes;
 
     private final SharedResourceHolder.Resource<ExecutorService> executorResource;
-    @GuardedBy("this")
-    private boolean shutdown;
+
+    /**
+     * 任务调度器
+     */
     @GuardedBy("this")
     private ExecutorService executor;
-    @GuardedBy("this")
-    private boolean resolving;
+
+    /**
+     * 监听器，更新地址
+     */
     @GuardedBy("this")
     private Listener listener;
 
-    public AddressChannelNameResolver(String name, GrpcChannelProperties properties, Attributes attributes, SharedResourceHolder.Resource<ExecutorService> executorResource) {
+    /**
+     * 解析器是否已经退出
+     */
+    @GuardedBy("this")
+    private boolean shutdown;
+
+    /**
+     * 是否正在解析 gRPC 服务器地址
+     */
+    @GuardedBy("this")
+    private boolean resolving;
+
+
+    /**
+     * 域名解析器构造器
+     *
+     * @param name 解析器名称
+     * @param properties 配置文件中配置的 RPC 服务器的信息
+     * @param attributes metadata 数据
+     * @param executorResource 一个 gRPC 实现的任务调度器
+     */
+    public AddressChannelNameResolver(String name,
+                                      GrpcChannelProperties properties,
+                                      Attributes attributes,
+                                      SharedResourceHolder.Resource<ExecutorService> executorResource) {
         this.name = name;
         this.properties = properties;
         this.attributes = attributes;
         this.executorResource = executorResource;
     }
 
+
+    /**
+     * 获取解析器的名称
+     *
+     * @return 解析器名称
+     */
     @Override
     public String getServiceAuthority() {
         return name;
     }
 
+
+    /**
+     * 开始解析
+     *
+     * @param listener 地址更新的监听器
+     */
     @Override
     public final synchronized void start(Listener listener) {
         Preconditions.checkState(this.listener == null, "already started");
@@ -54,23 +94,13 @@ public class AddressChannelNameResolver extends NameResolver {
         resolve();
     }
 
-    @SuppressWarnings("unchecked")
-    private void replace(List list, int max, Object defaultValue) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) == null) {
-                list.set(i, defaultValue);
-            }
-        }
-        for (int i = list.size(); i < max; i++) {
-            list.add(defaultValue);
-        }
-    }
 
     @Override
     public final synchronized void refresh() {
         Preconditions.checkState(listener != null, "not started");
         resolve();
     }
+
 
     private final Runnable resolutionRunnable = new Runnable() {
         @Override
@@ -83,24 +113,24 @@ public class AddressChannelNameResolver extends NameResolver {
                 savedListener = listener;
                 resolving = true;
             }
-            try {
-                int max = Math.max(properties.getHost().size(), properties.getPort().size());
-                replace(properties.getHost(), max, GrpcChannelProperties.DEFAULT_HOST);
-                replace(properties.getPort(), max, GrpcChannelProperties.DEFAULT_PORT);
 
+            try {
+                // 简单通过比较 host 和 post 的大小是否一致检测配置文件中的 gRPC 服务器中的配置是否错误
                 if (properties.getHost().size() != properties.getPort().size()) {
-                    log.error("config gRPC server {} error, hosts length isn't equals ports length,hosts [{}], ports [{}]", properties.getHost(), properties.getPort());
-                    savedListener.onError(Status.UNAVAILABLE.withCause(new RuntimeException("gRPC config error")));
+                    savedListener.onError(Status.UNAVAILABLE.withCause(new RuntimeException("gRPC 配置错误")));
                     return;
                 }
 
+                // 解析并添加 IP
                 List<EquivalentAddressGroup> equivalentAddressGroupList = Lists.newArrayList();
                 for (int i = 0; i < properties.getHost().size(); i++) {
                     String host = properties.getHost().get(i);
                     Integer port = properties.getPort().get(i);
-                    log.info("Found gRPC server {} {}:{}", name, host, port);
+                    log.debug("发现 gRPC 服务 {} {}:{}", name, host, port);
                     equivalentAddressGroupList.add(new EquivalentAddressGroup(new InetSocketAddress(host, port), Attributes.EMPTY));
                 }
+
+                // 更新 IP
                 savedListener.onAddresses(equivalentAddressGroupList, Attributes.EMPTY);
             } finally {
                 synchronized (AddressChannelNameResolver.this) {
@@ -110,6 +140,11 @@ public class AddressChannelNameResolver extends NameResolver {
         }
     };
 
+
+    /**
+     * 解析地址
+     *
+     */
     @GuardedBy("this")
     private void resolve() {
         if (resolving || shutdown) {
@@ -118,6 +153,11 @@ public class AddressChannelNameResolver extends NameResolver {
         executor.execute(resolutionRunnable);
     }
 
+
+    /**
+     * 关闭地址解析器
+     *
+     */
     @Override
     public void shutdown() {
         if (shutdown) {
--- grpc-client/src/main/java/com/grpc/client/controller/CommonController.java
+++ grpc-client/src/main/java/com/grpc/client/controller/CommonController.java
@@ -12,19 +12,19 @@ import java.util.Map;
 @RestController
 public class CommonController {
 
-//    private final TestServiceGrpcImpl testServiceGrpc;
-//
-//    @Autowired
-//    public CommonController(TestServiceGrpcImpl testServiceGrpc) {
-//        this.testServiceGrpc = testServiceGrpc;
-//    }
+    private final TestServiceGrpcImpl testServiceGrpc;
+
+    @Autowired
+    public CommonController(TestServiceGrpcImpl testServiceGrpc) {
+        this.testServiceGrpc = testServiceGrpc;
+    }
 
     @RequestMapping(value = "/grpc/client/common/test", method = RequestMethod.GET)
     public Map<String, Object> grpcTest() {
         Map<String, Object> map = new HashMap<>();
         map.put("code", 10000);
         map.put("msg", "success");
-//        testServiceGrpc.getUserInfo();
+        testServiceGrpc.getUserInfo();
         return map;
     }
 }
