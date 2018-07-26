88ebc4b5420541aaba45dcba8e8c6b1997cff31c
v_pwlu <v_pwlu@wesure.cn>
2018-07-26 20:44:24 +0800

    RestTemplate

---
+++ cloud-register/src/main/java/Application.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * company: wesure
+ * project Name: hello-grpc
+ *
+ * @author v_pwlu 2018/7/26
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
