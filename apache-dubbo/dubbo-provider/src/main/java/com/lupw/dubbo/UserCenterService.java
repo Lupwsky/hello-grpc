552e43acd91f4179906cc01ae7aa5d616e6fb81c
v_pwlu <v_pwlu@wesure.cn>
2018-08-06 20:44:28 +0800

    ribbon 基础例子

---
+++ apache-dubbo/dubbo-provider/src/main/java/com/lupw/dubbo/UserCenterService.java
@@ -0,0 +1,17 @@
+package com.lupw.dubbo;
+
+/**
+ * company: wesure
+ * project Name: hello-grpc
+ *
+ * @author v_pwlu 2018/8/1
+ */
+public interface UserCenterService {
+
+    /**
+     * 获取用户名
+     *
+     * @return
+     */
+    String getUsername();
+}
---
+++ apache-dubbo/dubbo-provider/src/main/java/com/lupw/dubbo/UserCenterServiceImpl.java
@@ -0,0 +1,15 @@
+package com.lupw.dubbo;
+
+/**
+ * company: wesure
+ * project Name: hello-grpc
+ *
+ * @author v_pwlu 2018/8/1
+ */
+public class UserCenterServiceImpl implements UserCenterService{
+
+    @Override
+    public String getUsername() {
+        return "lupengwei";
+    }
+}
