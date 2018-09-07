1d0c3991905528a749768e69cb7a17c9e46ad1fc
v_pwlu <v_pwlu@wesure.cn>
2018-09-07 19:40:51 +0800

    Dubbo

--- grpc-client/src/main/java/com/grpc/client/Application.java
+++ grpc-client/src/main/java/com/grpc/client/Application.java
@@ -6,12 +6,45 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Properties;
+
 @Slf4j
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-        log.info("应用已经启动");
+//        SpringApplication.run(Application.class, args);
+//        log.info("应用已经启动");
+
+
+        String classpath = System.getProperty("java.class.path");
+        log.info(classpath);
+
+        classpath = System.getProperty("sun.boot.class.path");
+        log.info(classpath);
+
+        classpath = System.getProperty("java.ext.dirs");
+        log.info(classpath);
+
+        URL uri = Application.class.getClassLoader().getResource("");
+        if (uri != null) {
+            log.info(uri.getPath());
+        } else {
+            throw new RuntimeException("无法获取资源文件的URI");
+        }
+
+        Properties properties = new Properties();
+        InputStream inputStream = Application.class.getClassLoader().getResourceAsStream("application.properties");
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        log.info(properties.getProperty("spring.application.name"));
     }
 }
