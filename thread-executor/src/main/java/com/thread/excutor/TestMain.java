afca99b623dd286db2e2836eecf51f01dbd4e330
v_pwlu <v_pwlu@wesure.cn>
2018-10-18 16:05:49 +0800

    thread-excutor

--- thread-executor/src/main/java/com/thread/excutor/TestMain.java
+++ thread-executor/src/main/java/com/thread/excutor/TestMain.java
@@ -4,7 +4,7 @@ import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.function.Function;
 
 /**
  * @author v_pwlu 2018/10/11
@@ -13,30 +13,22 @@ import java.util.stream.Collectors;
 public class TestMain {
     public static void main(String[] args) {
 
-        List<DataNode> dataNodeList1 = new ArrayList<>();
-        dataNodeList1.add(DataNode.builder().name("A").soc(1).build());
-        dataNodeList1.add(DataNode.builder().name("B").soc(1).build());
-        dataNodeList1.add(DataNode.builder().name("C").soc(1).build());
-        dataNodeList1.add(DataNode.builder().name("D").soc(1).build());
+        List<DataNode> dataNodeList = new ArrayList<>();
+        dataNodeList.add(DataNode.builder().name("A").soc(1).build());
+        dataNodeList.add(DataNode.builder().name("B").soc(2).build());
+        dataNodeList.add(DataNode.builder().name("C").soc(3).build());
+        dataNodeList.add(DataNode.builder().name("D").soc(4).build());
 
-        List<DataNode> dataNodeList2 = new ArrayList<>();
-        dataNodeList2.add(DataNode.builder().name("E").soc(1).build());
-        dataNodeList2.add(DataNode.builder().name("F").soc(1).build());
-        dataNodeList2.add(DataNode.builder().name("G").soc(1).build());
-        dataNodeList2.add(DataNode.builder().name("H").soc(1).build());
+        List<String> list = map(dataNodeList, DataNode::getName);
+        log.info(list.toString());
+    }
 
-        List<DataNode> dataNodeList3 = new ArrayList<>();
-        dataNodeList3.add(DataNode.builder().name("I").soc(1).build());
-        dataNodeList3.add(DataNode.builder().name("J").soc(1).build());
-        dataNodeList3.add(DataNode.builder().name("K").soc(1).build());
-        dataNodeList3.add(DataNode.builder().name("L").soc(1).build());
 
-        List<List<DataNode>> dataList = new ArrayList<>();
-        dataList.add(dataNodeList1);
-        dataList.add(dataNodeList2);
-        dataList.add(dataNodeList3);
-
-        List<DataNode> newDataList = dataList.stream().flatMap(Collection::stream).collect(Collectors.toList());
-        log.info(newDataList.toString());
+    private static List<String> map(List<DataNode> dataNodeList, Function<DataNode, String> function) {
+        List<String> returnDataList = new ArrayList<>();
+        for (DataNode dataNode : dataNodeList) {
+            returnDataList.add(function.apply(dataNode));
+        }
+        return returnDataList;
     }
 }
