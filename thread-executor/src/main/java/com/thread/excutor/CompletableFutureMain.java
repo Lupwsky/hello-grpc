b7c0b975fbdf91cb0f7179d76d815e3a5e4f8dea
v_pwlu <v_pwlu@wesure.cn>
2018-10-31 14:10:45 +0800

    CompletableFuture类使用示例

--- thread-executor/src/main/java/com/thread/excutor/CompletableFutureMain.java
+++ thread-executor/src/main/java/com/thread/excutor/CompletableFutureMain.java
@@ -14,20 +14,32 @@ public class CompletableFutureMain {
     public static void main(String[] args) {
         CompletableFuture<Integer> future1 = CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             return 100;
-        }).whenComplete((result, error) -> log.info("[Main] result = {}, error = {}", result, error.getMessage()));
+        });
 
-        try {
-            DateTime startTime = DateTime.now();
-            int finalResult = future1.get(10, TimeUnit.SECONDS);
-            DateTime endTime = DateTime.now();
-            log.info("[MAIN] finalResult = {}, time = {}ms", finalResult, endTime.getMillis() - startTime.getMillis());
-        } catch (InterruptedException | TimeoutException | ExecutionException e) {
-            e.printStackTrace();
-        }
+        CompletableFuture<Integer> future2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 200;
+        });
+
+        future1.runAfterEither(future2, () -> log.info("[MAIN] 所有计算完成")).join();
+
+//        // 不调用其中一个 future 的 get 方法, Runnable 的 run 方法就不会执行, 注释下面的代码, 会返现没有执行 run 方法
+//        try {
+//            DateTime startTime = DateTime.now();
+//            int finalResult = future2.get(10, TimeUnit.SECONDS);
+//            DateTime endTime = DateTime.now();
+//            log.info("[MAIN] finalResult = {}, time = {}ms", finalResult, endTime.getMillis() - startTime.getMillis());
+//        } catch (InterruptedException | TimeoutException | ExecutionException e) {
+//            e.printStackTrace();
+//        }
     }
 }
