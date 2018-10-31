01b569ae2bc240f712682a5fb8c84e6ca6eff078
v_pwlu <v_pwlu@wesure.cn>
2018-10-31 16:57:41 +0800

    Socket简单使用示例

--- socket-client/src/main/java/com/lupw/socket/SocketClientMain.java
+++ socket-client/src/main/java/com/lupw/socket/SocketClientMain.java
@@ -1,5 +1,7 @@
 package com.lupw.socket;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,36 +11,37 @@ import java.net.Socket;
 /**
  * @author v_pwlu 2018/9/30
  */
+@Slf4j
 public class SocketClientMain {
 
     public static void  main(String[] args) {
-
         Socket socket = null;
         try {
+            log.info("[客户端] 连接服务器");
             socket = new Socket("127.0.0.1", 8989);
 
             // 60s超时
-            socket.setSoTimeout(60000);
+            socket.setSoTimeout(60 * 1000);
 
             // 读取输入的数据
-            PrintWriter printWriter =new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader sysBuff =new BufferedReader(new InputStreamReader(System.in));
+            log.info("[客户端] 等待输入信息");
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader sysBuff = new BufferedReader(new InputStreamReader(System.in));
             printWriter.println(sysBuff.readLine());
-
-            // 刷新输出流, 使 server 马上收到该字符串
             printWriter.flush();
             sysBuff.close();
+            log.info("[客户端] 信息已发送");
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
                 if (socket != null) {
+                    log.info("[客户端] 断开连接");
                     socket.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
     }
 }
--- socket-server/src/main/java/com/lupw/socket/SocketServerMain.java
+++ socket-server/src/main/java/com/lupw/socket/SocketServerMain.java
@@ -15,26 +15,29 @@ import java.net.Socket;
 public class SocketServerMain {
 
     public static void main(String[] args) {
-
         ServerSocket serverSocket = null;
         try {
             // 创建一个 ServerSocket, 在端口 8989 监听客户请求
-            log.info("[服务端]");
-            serverSocket =new ServerSocket(8989);
+            log.info("[服务端] 服务端启动");
+            serverSocket = new ServerSocket(8989);
 
-            Socket socket = serverSocket.accept();
+            // 阻塞监听获取客户端发送的消息
+            Socket socket;
+            while (true){
+                socket = serverSocket.accept();
 
-            BufferedReader bufferedReader =new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String result = bufferedReader.readLine();
-            bufferedReader.close();
-            log.info("收到客户端消息 : {}", result);
-
-            socket.close();
+                // 读取从客户端发送的消息
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String result = bufferedReader.readLine();
+                bufferedReader.close();
+                log.info("[服务端] 收到消息 : {}", result);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
                 if (serverSocket != null) {
+                    log.info("[服务端] 服务端关闭");
                     serverSocket.close();
                 }
             } catch (IOException e) {
@@ -42,5 +45,4 @@ public class SocketServerMain {
             }
         }
     }
-
 }
