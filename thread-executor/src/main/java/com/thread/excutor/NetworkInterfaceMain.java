7612c85d5873ecb1290ae8cd60ca243d9e9ca564
v_pwlu <v_pwlu@wesure.cn>
2018-11-01 11:06:53 +0800

    Socket简单使用示例

--- thread-executor/src/main/java/com/thread/excutor/NetworkInterfaceMain.java
+++ thread-executor/src/main/java/com/thread/excutor/NetworkInterfaceMain.java
@@ -3,9 +3,7 @@ package com.thread.excutor;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
+import java.net.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
@@ -23,7 +21,7 @@ public class NetworkInterfaceMain {
     public static void main(String[] args) {
         try {
             Enumeration<NetworkInterface> networkInterfaceEnumeration = NetworkInterface.getNetworkInterfaces();
-            // 通常情况下, 即使主机没有任何其他网络连接, 回环接口也总是存在的
+            // 通常情况下, 即使主机没有任何其他网络连接, 回环接口也总是存在的, IPv4 = 127.0.0.1, IPv6 = 0:0:0:0:0:0:0:1
             if (networkInterfaceEnumeration == null) {
                 log.info("[获取IP信息] 没有获取到IP信息");
             } else {
@@ -32,16 +30,52 @@ public class NetworkInterfaceMain {
                     // 网络名称
                     String networkName = networkInterface.getName();
 
-                    List<String> addressList = new ArrayList<>();
+                    // IP 版本
+                    String ipVersion = "NONE";
+                    List<String> hostNameList = new ArrayList<>();
+                    List<String> addressList  = new ArrayList<>();
                     Enumeration<InetAddress> inetAddressEnumeration = networkInterface.getInetAddresses();
                     while(inetAddressEnumeration.hasMoreElements()) {
                         InetAddress inetAddress = inetAddressEnumeration.nextElement();
-                        addressList.add(inetAddress.getHostName());
+                        if (inetAddress instanceof Inet4Address) {
+                            ipVersion = "v4";
+                        } else if(inetAddress instanceof Inet6Address) {
+                            ipVersion = "v6";
+                        } else {
+                            ipVersion = "NONE";
+                        }
+                        hostNameList.add(inetAddress.getHostName());
+
+                        // IPv4 是点分形式, IPv6 是冒号分隔的 16 进制形式
+                        addressList.add(inetAddress.getHostAddress());
                     }
-                    log.info("[获取IP信息] name = {}, address = {}", networkName, addressList.toString());
+                    log.info("[获取IP信息] ipVersion = {}, networkName = {}, hostName = {}, address = {}",
+                            ipVersion, networkName, hostNameList.toString(), addressList.toString());
                 }
             }
         } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        // 通过 hostName 获取地址信息
+        String hostName = "www.baidu.com";
+        try {
+            String ipVersion = "NONE";
+            List<String> addressList  = new ArrayList<>();
+            InetAddress[] inetAddressList = InetAddress.getAllByName(hostName);
+            for (InetAddress inetAddress : inetAddressList) {
+                if (inetAddress instanceof Inet4Address) {
+                    ipVersion = "v4";
+                } else if(inetAddress instanceof Inet6Address) {
+                    ipVersion = "v6";
+                } else {
+                    ipVersion = "NONE";
+                }
+                addressList.add(inetAddress.getHostAddress());
+            }
+            log.info("[获取IP信息] ipVersion = {}, hostName = {}, address = {}",
+                    ipVersion, hostName, addressList.toString());
+        } catch (UnknownHostException e) {
             e.printStackTrace();
         }
     }
