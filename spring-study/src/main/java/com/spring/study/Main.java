a8c9bca15f46d29eb88895659f82f766c313571e
v_pwlu <v_pwlu@wesure.cn>
2019-04-09 16:14:50 +0800

    近期笔记

--- spring-study/src/main/java/com/spring/study/Main.java
+++ spring-study/src/main/java/com/spring/study/Main.java
@@ -1,6 +1,10 @@
 package com.spring.study;
 
+import com.alibaba.fastjson.JSONObject;
+import com.spring.study.beans.UserInfo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * @author v_pwlu 2019/3/25
@@ -9,21 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
 
     public static void main(String[] args) {
-        // count++ 和 ++count, 先使用, 再加 1, 先加 1, 再使用,
-        // 这里的使用不是使用 count 值, 使用的是 count++ 这个表达式的值, count++ 和 ++count 表达式不仅仅是让 count 自增, 并且是有返回值的, count++ 返回的是 count 自增之前的值, ++count 返回的是 count 自增后的值
-        // 对于 count = count++ 这个表达式, count 先自增 1, count 的值为 1, 然后将 count++ 表达式的返回的值赋值给 count,
-        // count++ 返回的是 count 自增前的值, 自增前值为 0, 因此 count 虽然在前面自增了 1 , 但是在这一步又被赋值为 0, 因此循环结束后 count 的值还是 0
-
-        int count = 0;
-        for (int i = 0; i < 10; i++) {
-            count = count++;
-        }
-
-        // 对于表达式 count++, JVM 做的操作是如下:
-        // *　JVM 把 count 值拷贝到临时变量中
-        // *　JVM 将 count 值加 1
-        // *　JVM 返回临时变量的值
-
-        // 不过, 现在 IDE 都很智能, 像 count = count++ 这种写法都会告警提示的, 我觉得实际项目中应该很少有人会这样写吧~~
+        XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("/beans/UserBeans.xml"));
+        UserInfo userInfo = beanFactory.getBean("userInfo", UserInfo.class);
+        log.info("{}", JSONObject.toJSONString(userInfo));
+        System.exit(0);
     }
 }
