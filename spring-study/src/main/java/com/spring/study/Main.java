86161b26d8583b5f45f4c43f35b958e32b09655a
v_pwlu <v_pwlu@wesure.cn>
2019-04-18 16:33:03 +0800

    近期笔记

--- spring-study/src/main/java/com/spring/study/Main.java
+++ spring-study/src/main/java/com/spring/study/Main.java
@@ -2,8 +2,11 @@ package com.spring.study;
 
 import com.alibaba.fastjson.JSONObject;
 import com.spring.study.beans.UserInfo;
+import com.spring.study.beans.UserInfoA;
+import com.spring.study.beans.UserInfoB;
 import lombok.extern.slf4j.Slf4j;
 
+import org.javatuples.Pair;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 
@@ -18,6 +21,19 @@ public class Main {
         XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("/beans/UserBeans.xml"));
         UserInfo userInfo = (UserInfo) beanFactory.getBean("userInfo", "lupengwei", "lupengwei@qq.com");
         log.info("{}", JSONObject.toJSONString(userInfo));
-        System.exit(0);
+
+        Pair<UserInfoA, UserInfoB> userInfoBPair = getUserInfoBPair();
+        UserInfoA userInfoA = userInfoBPair.getValue0();
+        UserInfoB userInfoB = userInfoBPair.getValue1();
+        log.info("userInfoA = {}, userInfoB = {}", JSONObject.toJSONString(userInfoA), JSONObject.toJSONString(userInfoB));
+    }
+
+
+    /**
+     * https://www.javatuples.org/
+     */
+    private static Pair<UserInfoA, UserInfoB> getUserInfoBPair() {
+        return Pair.with(UserInfoA.builder().userInfoA("A").build(),
+                UserInfoB.builder().userInfoB("B").build());
     }
 }
