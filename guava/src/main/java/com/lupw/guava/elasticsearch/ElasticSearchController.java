049bee464e15084120567c564bcc8a10a57a2751
v_pwlu <v_pwlu@wesure.cn>
2019-04-24 17:42:27 +0800

    近期笔记

--- guava/src/main/java/com/lupw/guava/elasticsearch/ElasticSearchController.java
+++ guava/src/main/java/com/lupw/guava/elasticsearch/ElasticSearchController.java
@@ -2,19 +2,24 @@ package com.lupw.guava.elasticsearch;
 
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
+import org.elasticsearch.client.core.TermVectorsRequest;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.get.GetResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -24,6 +29,8 @@ import java.util.Map;
 @RestController
 public class ElasticSearchController {
 
+    private final static String ES_INDEX = "user_data";
+    private final static String ES_TYPE = "user_info";
     private final RestHighLevelClient client;
 
     @Autowired
@@ -31,16 +38,17 @@ public class ElasticSearchController {
         this.client = client;
     }
 
-    // (0) 基础概念 = https://xiangzhurui.com/2018/03/07/Elasticsearch-%E5%85%A5%E9%97%A8-%E5%9F%BA%E7%A1%80%E6%A6%82%E5%BF%B5/
-    // (1) Index 和 Type 的区别 = http://bayescafe.com/database/elasticsearch-using-index-or-type.html
-
+    /**
+     * 官方文档 = https://www.elastic.co/guide/en/elasticsearch/client/java-rest/6.6/java-rest-high-document-index.html
+     * Index 和 Type 的区别 = http://bayescafe.com/database/elasticsearch-using-index-or-type.html
+     */
     @GetMapping("/elasticsearch/create/index/test")
     public void createIndexTest() {
         Map<String, Object> jsonMap = Maps.newHashMap();
         jsonMap.put("name", "lpw");
         jsonMap.put("password", "test");
 
-        IndexRequest indexRequest = new IndexRequest("user_data", "user_info", "1");
+        IndexRequest indexRequest = new IndexRequest(ES_INDEX, ES_TYPE, "1");
         indexRequest.source(jsonMap);
 
         try {
@@ -50,9 +58,12 @@ public class ElasticSearchController {
         }
     }
 
+    /**
+     * 官方文档 = https://www.elastic.co/guide/en/elasticsearch/client/java-rest/6.6/java-rest-high-document-get.html
+     */
     @GetMapping("/elasticsearch/get/request/test")
     public void getRequestTest() {
-        GetRequest getRequest = new GetRequest("user_data", "user_info", "1");
+        GetRequest getRequest = new GetRequest(ES_INDEX, ES_TYPE, "1");
         try {
             GetResponse getResponse = client.get(getRequest, RequestOptions.DEFAULT);
             log.info("index = {}", getResponse.getIndex());
@@ -62,5 +73,68 @@ public class ElasticSearchController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 官方文档 = https://www.elastic.co/guide/en/elasticsearch/client/java-rest/6.6/java-rest-high-document-exist.html
+     */
+    @GetMapping("/elasticsearch/exist/request/test")
+    public void existRequestTest() {
+        GetRequest getRequest = new GetRequest(ES_INDEX, ES_TYPE, "1");
+        try {
+            boolean isExist = client.exists(getRequest, RequestOptions.DEFAULT);
+            log.info("isExist = {}", isExist);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 官方文档 = https://www.elastic.co/guide/en/elasticsearch/client/java-rest/6.6/java-rest-high-document-delete.html
+     */
+    @GetMapping("/elasticsearch/delete/request/test")
+    public void deleteRequestTest() {
+        DeleteRequest deleteRequest = new DeleteRequest(ES_INDEX, ES_TYPE, "2");
+        try {
+            DeleteResponse deleteResponse = client.delete(deleteRequest, RequestOptions.DEFAULT);
+            log.info("deleteStatus = {}", deleteResponse.status().getStatus());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 官方文档 = https://www.elastic.co/guide/en/elasticsearch/client/java-rest/6.6/java-rest-high-document-update.html
+     */
+    @GetMapping("/elasticsearch/update/request/test")
+    public void updateRequestTest() {
+        UpdateRequest updateRequest = new UpdateRequest(ES_INDEX, ES_TYPE, "1");
+        try {
+            XContentBuilder builder = XContentFactory.jsonBuilder();
+            builder.startObject();
+            {
+                builder.field("name", "lpw123");
+                builder.field("password", "lpw123");
+            }
+            builder.endObject();
+
+            UpdateResponse updateResponse = client.update(updateRequest, RequestOptions.DEFAULT);
+            GetResult getResult = updateResponse.getGetResult();
+            if (getResult.isExists()) {
+                log.info("source = {}", getResult.getSource());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 功能 = 返回特定文档字段中术语的信息和统计信息
+     * 官方文档 = https://www.elastic.co/guide/en/elasticsearch/client/java-rest/6.6/java-rest-high-document-term-vectors.html
+     */
+    @GetMapping("/elasticsearch/term/request/test")
+    public void termVectorsRequestTest() {
+        TermVectorsRequest termVectorsRequest = new TermVectorsRequest(ES_INDEX, ES_TYPE, "1");
+        termVectorsRequest.setFields("name", "password");
     }
 }
