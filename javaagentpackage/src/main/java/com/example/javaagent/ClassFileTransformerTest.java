06acd0259781e9ef74cded50aa88d747e3c8da82
Lupw <2244023719@qq.com>
2019-05-27 23:28:07 +0800

    近期笔记

--- javaagentpackage/src/main/java/com/example/javaagent/ClassFileTransformerTest.java
+++ javaagentpackage/src/main/java/com/example/javaagent/ClassFileTransformerTest.java
@@ -1,10 +1,12 @@
 package com.example.javaagent;
 
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.NotFoundException;
+import javassist.*;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
@@ -21,16 +23,52 @@ public class ClassFileTransformerTest implements ClassFileTransformer {
                             Class<?> classBeingRedefined,
                             ProtectionDomain protectionDomain,
                             byte[] classfileBuffer) throws IllegalClassFormatException {
-        if (className.startsWith("com/example")) {
+        if (!className.startsWith("com/example")) {
+            return null;
+        } else {
             log.info("className = {}", className);
+
+            String fullClassName = className.replace("/", ".");
+            log.info("fullClassName = {}", fullClassName);
+
             ClassPool classPool = ClassPool.getDefault();
+
+            log.info("classPool = {}", "classPool");
+            CtClass ctClass = null;
             try {
-                CtClass ctClass = classPool.getCtClass(className.replace("/", "\\."));
-                log.info("ctClass = {}", ctClass);
+                ctClass = classPool.get(fullClassName);
             } catch (NotFoundException e) {
-                log.error("exception = {}, error = {}", "NotFoundException", e);
+                // Spring 和有可能通过字节码对我们的类进行扩展, 这种情况下找不到对应的class文件, 会报NotFoundException
+                // 例如com/example/javaagent/JavaagentApplication$$EnhancerBySpringCGLIB$$b02db345
+                // com.example.javaagent.JavaagentApplication$$EnhancerBySpringCGLIB$$b02db345
+                // 直接读取classfileBuffer, 直接通过类的字节码创建CtClass对象
+                try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(classfileBuffer)) {
+                    ctClass = classPool.makeClass(byteArrayInputStream);
+                } catch (IOException ex) {
+                    log.error("exception = {}, error = {}", "IOException", ex);
+                }
+            }
+
+            // 接口类不做处理
+            if (ctClass == null || ctClass.isInterface()) {
+                return null;
+            }
+
+            // 在原方法体前面和后面添加代码
+            try {
+                CtMethod[] methods = ctClass.getDeclaredMethods();
+                for (CtMethod ctMethod : methods) {
+                    ctMethod.insertBefore("log.info(\"transform begin...\")");
+                    ctMethod.insertAfter("log.info(\"transform end...\")");
+                }
+                return ctClass.toBytecode();
+            } catch (CannotCompileException | IOException e) {
+                log.error("exception = {}, error = {}", "CannotCompileException", e);
+                return null;
+            } finally {
+                // 清除缓存
+                ctClass.detach();
             }
         }
-        return new byte[0];
     }
 }
