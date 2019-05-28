641025a0ef116da7d473f789cd1aea0bfeeccb2f
v_pwlu <v_pwlu@wesure.cn>
2019-05-28 11:55:00 +0800

    近期笔记

--- javaagentpackage/src/main/java/com/example/javaagent/ClassFileTransformerTest.java
+++ javaagentpackage/src/main/java/com/example/javaagent/ClassFileTransformerTest.java
@@ -1,8 +1,6 @@
 package com.example.javaagent;
 
 import javassist.*;
-import javassist.expr.ExprEditor;
-import javassist.expr.MethodCall;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.ByteArrayInputStream;
@@ -10,6 +8,7 @@ import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
+import java.util.Arrays;
 
 /**
  * @author v_pwlu 2019/5/27
@@ -38,10 +37,10 @@ public class ClassFileTransformerTest implements ClassFileTransformer {
             try {
                 ctClass = classPool.get(fullClassName);
             } catch (NotFoundException e) {
-                // Spring 和有可能通过字节码对我们的类进行扩展, 这种情况下找不到对应的class文件, 会报NotFoundException
-                // 例如com/example/javaagent/JavaagentApplication$$EnhancerBySpringCGLIB$$b02db345
+                // Spring 框架很有可能对我们的类进行扩展生成代理类, 这种情况下会出现找不到对应的 class 文件, 会出现 NotFoundException
+                // 例如 com/example/javaagent/JavaagentApplication$$EnhancerBySpringCGLIB$$b02db345
                 // com.example.javaagent.JavaagentApplication$$EnhancerBySpringCGLIB$$b02db345
-                // 直接读取classfileBuffer, 直接通过类的字节码创建CtClass对象
+                // 可以直接读取 classfileBuffer, 直接通过类的字节码创建CtClass对象, 或者简单点直接通过判断是否包含有 $ 符号直接判定
                 try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(classfileBuffer)) {
                     ctClass = classPool.makeClass(byteArrayInputStream);
                 } catch (IOException ex) {
@@ -56,14 +55,20 @@ public class ClassFileTransformerTest implements ClassFileTransformer {
 
             // 在原方法体前面和后面添加代码
             try {
+                Object[] annotations = ctClass.getAnnotations();
+                log.info("className = {}, annotations = {}", className, Arrays.toString(annotations));
                 CtMethod[] methods = ctClass.getDeclaredMethods();
                 for (CtMethod ctMethod : methods) {
-                    ctMethod.insertBefore("log.info(\"transform begin...\")");
-                    ctMethod.insertAfter("log.info(\"transform end...\")");
+                    log.info("className = {}, methodName = {}", className, ctMethod.getName());
+                    ctMethod.insertBefore("System.out.println(\"" + className + " transform begin...\");");
+                    ctMethod.insertAfter("System.out.println(\"" + className + " transform end...\");");
                 }
                 return ctClass.toBytecode();
             } catch (CannotCompileException | IOException e) {
                 log.error("exception = {}, error = {}", "CannotCompileException", e);
+                return null;
+            } catch (ClassNotFoundException e) {
+                log.warn("Slf4j 类不存在");
                 return null;
             } finally {
                 // 清除缓存
